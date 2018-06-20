b8bc66a9e88f3dcd6d28d1428582a119f81b2ae4
Vidit7792 <39307865+Vidit7792@users.noreply.github.com>
2018-06-20 22:28:10 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,24 @@
+var recipeName=('Creamy squash linguine');
+var preparationTime =('45');
+var recipeBy=('Jimmy Cutter');
+var isVegetarian=('True');
+var Calories=('223.7');
+console.log(recipeName)
+console.log(preparationTime)
+console.log(recipeBy)
+console.log(isVegetarian)
+console.log(Calories)
+
+
+var recipeName=('Creamy squash linguine');
+var preparationTime =('45');
+preparationTime=('60')
+var recipeBy=('Jimmy Cutter');
+recipeBy=('Jimmy Cutter George')
+var isVegetarian=('True');
+var Calories=('223.7');
+console.log(recipeName)
+console.log(preparationTime)
+console.log(recipeBy)
+console.log(isVegetarian)
+console.log(Calories)
